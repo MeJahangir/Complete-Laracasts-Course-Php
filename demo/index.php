4b830354dfa3191ae06011981d68141c7aada39a
MD JAHANGIR ALAM <you@example.com>
2024-11-15 13:19:31 +0600

    submit php demo files

---
+++ demo/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <?Php
+
+        $name = 'md jahangir alam';
+        $read = true;
+
+        if ($read) {
+            $masage = "You Have Read $name";
+        }else{
+            $masage = "You Have Not Read $name";
+        }
+    ?>
+
+    <h1>
+        <?php echo $masage; ?>
+    </h1>
+</body>
+</html>
